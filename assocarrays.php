d444c43cd03c57ab10f64674652499d2d162518e
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-01 16:36:37 -0600

    renamed assocarrays to assocarrays.php created assocrays

---
+++ assocarrays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+	<title>Associative Arrays</title>
+	</head>
+	<body>
+
+		<?php $assoc = array("first_name" => "Nick", "last_name" => "Pascetti");
+		?>
+
+		<?php echo $assoc["first_name"]; ?><br />
+
+	</body>
+</html>
