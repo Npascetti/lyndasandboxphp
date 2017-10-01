97615af09fe7e3acb0f3152b6158421dbe8af597
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-01 17:52:59 -0600

    finished booleans finished array functions, started booleans finished assocarrays renamed assocarrays to assocarrays.php created assocrays

--- booleans.php
+++ booleans.php
@@ -7,6 +7,18 @@
 		<?php
 		  $result1 = true;
 		  $result2 = false;
+		?>
+		Result1: <?php echo $result1; ?><br />
+		Result2: <?php echo $result2; ?><br />
+
+		result2 is boolean? <?php echo is_bool($result2); ?><br />
+		<br />
+
+		<?php $number = 3;
+		if(is_float($number) ) {
+			echo "It is a float";
+		}
+		?>
 
 	</body>
 </html>
